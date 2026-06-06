3d2659cba2f282f0d062ae3a4e8b0c0da7dde717
Naimur00998 <hemiliame.service0099@gmail.com>
2026-06-06 17:59:39 +0600

    server.js

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
-
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
@@ -18,13 +17,23 @@ io.on('connection', (socket) => {
     clients[socket.id] = {
       id: socket.id,
       name: data.name,
-      device: data.device
+      device: data.device,
+      battery: data.battery || 0,
+      network: data.network || 'unknown',
+      isCharging: data.isCharging || false
     };
     console.log('Client connected:', data.name);
-    // Notify all admins
-    Object.keys(admins).forEach(adminId => {
-      io.to(adminId).emit('client_list', Object.values(clients));
-    });
+    broadcastClientList();
+  });
+
+  // Client info update (battery, network)
+  socket.on('client_info', (data) => {
+    if (clients[socket.id]) {
+      clients[socket.id].battery = data.battery;
+      clients[socket.id].network = data.network;
+      clients[socket.id].isCharging = data.isCharging;
+      broadcastClientList();
+    }
   });
 
   // Admin register
@@ -37,6 +46,11 @@ io.on('connection', (socket) => {
   // Admin requests screen
   socket.on('request_screen', (clientId) => {
     io.to(clientId).emit('start_screen');
+  });
+
+  // Admin stops screen
+  socket.on('stop_screen', (clientId) => {
+    io.to(clientId).emit('stop_screen');
   });
 
   // Client sends screen data
@@ -58,15 +72,47 @@ io.on('connection', (socket) => {
     });
   });
 
+  // Admin launches app on client
+  socket.on('launch_app', (data) => {
+    io.to(data.clientId).emit('launch_app', { package: data.package });
+  });
+
+  // Admin requests app list
+  socket.on('get_app_list', (clientId) => {
+    io.to(clientId).emit('get_app_list');
+  });
+
+  // Client sends app list
+  socket.on('app_list', (data) => {
+    Object.keys(admins).forEach(adminId => {
+      io.to(adminId).emit('app_list', {
+        clientId: socket.id,
+        apps: data.apps
+      });
+    });
+  });
+
+  // File transfer — admin to client
+  socket.on('file_transfer', (data) => {
+    io.to(data.clientId).emit('file_receive', {
+      fileName: data.fileName,
+      fileData: data.fileData,
+      mimeType: data.mimeType
+    });
+  });
+
   // Disconnect
   socket.on('disconnect', () => {
     delete clients[socket.id];
     delete admins[socket.id];
+    broadcastClientList();
+  });
+
+  function broadcastClientList() {
     Object.keys(admins).forEach(adminId => {
       io.to(adminId).emit('client_list', Object.values(clients));
     });
-  });
-
+  }
 });
 
 app.get('/', (req, res) => {
